441bbe07cad1ed76d792cbd82c4fa4a55a54adb6
eoliva50 <eoliva50>
2023-10-16 01:28:31 -0500

    Added Questions

---
+++ src/components/MainLayout.vue
@@ -0,0 +1,22 @@
+<script setup>
+import MainScore from '@/components/MainScore.vue'
+</script>
+
+<template>
+  <main>
+    <div class="card">
+      <RouterView />
+    </div>
+  </main>
+  <MainScore />
+</template>
+
+<style lang="postcss" scoped>
+main {
+  @apply flex h-screen items-center justify-center bg-gradient-to-b from-gray-900 to-gray-600;
+
+  & .card {
+    @apply flex h-5/6 min-h-fit w-11/12 flex-col items-center gap-16 overflow-auto rounded-lg bg-white p-8 shadow-lg;
+  }
+}
+</style>
